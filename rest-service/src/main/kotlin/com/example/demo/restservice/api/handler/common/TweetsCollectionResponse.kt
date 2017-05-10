9a91039db1df0c56ad36f172626696b09542a6c0
bastman <gtgames88>
2017-05-10 08:38:04 +0200

    codeformatter applied

--- rest-service/src/main/kotlin/com/example/demo/restservice/api/handler/common/TweetsCollectionResponse.kt
+++ rest-service/src/main/kotlin/com/example/demo/restservice/api/handler/common/TweetsCollectionResponse.kt
@@ -2,7 +2,7 @@ package com.example.demo.restservice.api.handler.common
 
 import com.example.demo.restservice.domain.Tweet
 
-data class TweetsCollectionResponse(val tweets:List<Tweet>) {
+data class TweetsCollectionResponse(val tweets: List<Tweet>) {
     companion object {
         fun of(tweets: Sequence<Tweet>) = TweetsCollectionResponse(tweets = tweets.toList())
     }
