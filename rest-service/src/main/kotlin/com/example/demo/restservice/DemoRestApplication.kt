9e8268d2df694a66ed30fa0da35f7a3eea875f0a
bastman <gtgames88>
2017-05-10 08:14:32 +0200

    add example tweets to repository on startup

--- rest-service/src/main/kotlin/com/example/demo/restservice/DemoRestApplication.kt
+++ rest-service/src/main/kotlin/com/example/demo/restservice/DemoRestApplication.kt
@@ -1,12 +1,15 @@
 package com.example.demo.restservice
 
 import com.example.demo.logging.AppLogger
+import com.example.demo.restservice.domain.Tweet
+import com.example.demo.restservice.domain.TweetService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.Bean
+import java.time.Instant
 
 @SpringBootApplication
 open class DemoRestApplication {
@@ -25,8 +28,14 @@ open class DemoRestApplication {
     @Bean
     open fun init(
             ctx: ConfigurableApplicationContext,
-            @Value("\${app.appName}") appName: String
+            @Value("\${app.appName}") appName: String,
+            tweetService: TweetService
     ) = CommandLineRunner {
         LOGGER.info("=== init $appName - $ctx =====")
+
+        listOf<Tweet>(
+                Tweet(id = "1", author = "seb", message = "message 1", createdAt = Instant.now()),
+                Tweet(id = "2", author = "seb", message = "message 1", createdAt = Instant.now())
+        ).forEach { tweetService.submit(it) }
     }
 }
